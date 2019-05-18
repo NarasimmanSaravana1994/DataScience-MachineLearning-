f255212cf8012bf9fe1f73ffdd950a91fc4cd3f6
NarasimmanSaravana1994 <41271370+NarasimmanSaravana1994@users.noreply.github.com>
2019-05-19 02:36:54 +0530

    correlation
    correlation fucntion intial commit.

---
+++ correlation.py
@@ -0,0 +1,33 @@
+import math
+
+#Corelation formula 
+def Internal_correlation(x,y):
+    xy=[]
+    for index,value in enumerate(x):
+        xy.append(value * y[index])
+    X=[]
+    for value in x:
+        X.append(value*value)
+    Y=[]
+    for value in y:
+        Y.append(value*value)
+        
+    return(((sum(xy)*6)-(sum(x)*sum(y)))/math.sqrt(((sum(X)*6)-(sum(x)*sum(x)))*((sum(Y)*6)-(sum(y)*sum(y)))))
+    
+    
+    
+#Multiple argument handeling for y
+def Correlation(x,y):
+    for value in y:
+        print(Internal_correlation(x,value))
+    
+    
+
+#Sample arguments
+x = [43,21,25,42,57,59]
+y = [[99,65,79,75,87,81],[99,65,79,75,87,81],[99,65,79,75,87,81]]
+
+
+
+# Calling the fucntion
+Correlation(x,y)
